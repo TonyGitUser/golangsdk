6e089b3ece57d010d52e488afc0ca9048a610ec4
Eric Y <EricYangQM@outlook.com>
2018-06-24 02:09:18 +0800

    Fix format error.

--- openstack/as/v1/groups/testing/requests_test.go
+++ openstack/as/v1/groups/testing/requests_test.go
@@ -15,32 +15,32 @@ func TestCreate(t *testing.T) {
 
 	tenantId := "57e98940a77f4bb988a21a7d0603a626"
 	actual, err := groups.Create(client.ServiceClient(), tenantId, groups.CreateOpts{
-		ScalingGroupName:       "as-group-Test",
-		ScalingConfigurationId: "f109bb4f-09f0-4dac-9115-6b429d548750",
-		DesireInstanceNumber:   0,
-		MinInstanceNumber:      0,
-		MaxInstanceNumber:      1,
-		CoolDownTime:           900,
-		LbListenerId:           "",
-		Networks: []struct {
-			// Specifies the network ID.
-			Id string `json:"id,omitempty"`
-		}{
-			{Id: "f6a0db7b-397c-4162-bc35-9a1f008b4373"},
-		},
-		AvailableZones: []string{"cn-north-1a"},
-		SecurityGroups: []struct {
-			// Specifies the ID of the security group.
-			Id string `json:"id,omitempty"`
-		}{
-			{Id: "7844d4b4-d78f-45dc-9465-2b4d1bca83a5"},
-		},
+		ScalingGroupName:          "as-group-Test",
+		ScalingConfigurationId:    "f109bb4f-09f0-4dac-9115-6b429d548750",
+		DesireInstanceNumber:      0,
+		MinInstanceNumber:         0,
+		MaxInstanceNumber:         1,
+		CoolDownTime:              900,
+		LbListenerId:              "",
 		VpcId:                     "773c3c42-d315-417b-9063-87091713148c",
 		HealthPeriodicAuditMethod: "NOVA_AUDIT",
 		HealthPeriodicAuditTime:   5,
 		InstanceTerminatePolicy:   "OLD_CONFIG_OLD_INSTANCE",
 		Notifications:             []string{},
 		DeletePublicip:            true,
+		AvailableZones:            []string{"cn-north-1a"},
+		Networks: []struct {
+			// Specifies the network ID.
+			Id string `json:"id,omitempty"`
+		}{
+			{Id: "f6a0db7b-397c-4162-bc35-9a1f008b4373"},
+		},
+		SecurityGroups: []struct {
+			// Specifies the ID of the security group.
+			Id string `json:"id,omitempty"`
+		}{
+			{Id: "7844d4b4-d78f-45dc-9465-2b4d1bca83a5"},
+		},
 	}).Extract()
 	th.AssertNoErr(t, err)
 	th.CheckDeepEquals(t, &CreateResponse, actual)
